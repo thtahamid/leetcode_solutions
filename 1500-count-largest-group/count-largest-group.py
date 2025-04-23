5274d421133b55f9f55378579121177361bd04b2
Tahamid Hossain <mitahamid13@gmail.com>
2025-04-23 11:20:22 +0400

    Time: 72 ms (11.75%) | Memory: 17.6 MB (99.25%) - LeetSync

---
+++ 1500-count-largest-group/count-largest-group.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countLargestGroup(self, n: int) -> int:
+        hashMap = collections.Counter()
+        for i in range(1, n + 1):
+            key = sum([int(x) for x in str(i)])
+            hashMap[key] += 1
+        maxValue = max(hashMap.values())
+        count = sum(1 for v in hashMap.values() if v == maxValue)
+        return count
