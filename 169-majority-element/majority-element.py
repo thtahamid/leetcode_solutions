bde4836a6483cfd3e7d37a9b44b8f7d6ca2fe3e9
Tahamid Hossain <mitahamid13@gmail.com>
2025-05-05 01:48:15 +0400

    Time: 0 ms (100.00%) | Memory: 19.3 MB (87.29%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,28 +1,5 @@
-from typing import List
-
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        def countmajor(arr, num, left, right):
-            count = 0
-            for i in range(left, right + 1):
-                if arr[i] == num:
-                    count += 1
-            return count
-
-        def mej(arr, left, right):
-            if left == right:
-                return arr[left]
-            
-            mid = (left + right) // 2
-            leftMajor = mej(arr, left, mid)
-            rightMajor = mej(arr, mid + 1, right)
-
-            if leftMajor == rightMajor:
-                return leftMajor
-
-            leftcount = countmajor(arr, leftMajor, left, right)
-            rightcount = countmajor(arr, rightMajor, left, right)
-
-            return leftMajor if leftcount > rightcount else rightMajor
-
-        return mej(nums, 0, len(nums) - 1)
+        nums.sort()
+        n = len(nums)
+        return nums[n//2]
