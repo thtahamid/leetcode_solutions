9f4fb231984f0bd5a7bf71b39613052bf94b8c80
Tahamid Hossain <mitahamid13@gmail.com>
2025-07-21 13:54:13 +0400

    Time: 279 ms (86.88%) | Memory: 19.4 MB (76.94%) - LeetSync

--- 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.py
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.py
@@ -1,13 +1,18 @@
 class Solution:
     def makeFancyString(self, s: str) -> str:
+
+        prev = s[0]
+        frequency = 1
         ans = s[0]
-        cnt = 1
+
         for i in range(1, len(s)):
-            if s[i] == ans[-1]:
-                cnt += 1
-                if cnt < 3:
-                    ans += s[i]
+            if s[i] == prev:
+                frequency += 1
             else:
-                cnt = 1
+                prev = s[i]
+                frequency = 1
+
+            if frequency < 3:
                 ans += s[i]
+
         return ans
