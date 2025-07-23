c1bfe881697760b9fbfa2c9fd0dc7ed48bb65ea7
Tahamid Hossain <mitahamid13@gmail.com>
2025-07-23 21:46:20 +0400

    Time: 235 ms (47.74%) | Memory: 19.3 MB (67.10%) - LeetSync

--- 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.py
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.py
@@ -1,29 +1,22 @@
-class Solution(object):
-    def maximumGain(self, s, x, y):
-        def remove_pairs(s, pair, score):
-            res = 0
+class Solution:
+    def maximumGain(self, s: str, x: int, y: int) -> int:
+        
+        def solve(text: str, pattern: str, score: int):
             stack = []
-            for c in s:
-                if c == pair[1] and stack and stack[-1] == pair[0]:
+            points = 0
+            for char in text:
+                if stack and stack[-1] == pattern[0] and char == pattern[1]:
                     stack.pop()
-                    res += score
+                    points += score
                 else:
-                    stack.append(c)
-            return res, "".join(stack)
-        
-        res = 0
-        pair = "ab" if x > y else "ba"
-        score = max(x, y)
-        min_score = min(x, y)
-        
-        # Remove pairs with higher score first
-        gain, s = remove_pairs(s, pair, score)
-        res += gain
-        
-        # Remove pairs with lower score next
-        gain, s = remove_pairs(s, pair[::-1], min_score)
-        res += gain
-        
-        return res
+                    stack.append(char)
+            return points, "".join(stack)
 
-        
+        if x >= y:
+            points1, remaining_s = solve(s, "ab", x)
+            points2, _ = solve(remaining_s, "ba", y)
+        else:
+            points1, remaining_s = solve(s, "ba", y)
+            points2, _ = solve(remaining_s, "ab", x)
+            
+        return points1 + points2
