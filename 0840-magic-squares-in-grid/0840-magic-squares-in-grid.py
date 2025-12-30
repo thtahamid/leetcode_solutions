2268fc0bc28393ce9d94f199f5b33fd8273348f0
Tahamid Hossain <mitahamid13@gmail.com>
2025-12-30 23:50:44 +0400

    Time: 3 ms (44.58%), Space: 17.1 MB (100%) - LeetHub

--- 0840-magic-squares-in-grid/0840-magic-squares-in-grid.py
+++ 0840-magic-squares-in-grid/0840-magic-squares-in-grid.py
@@ -1,58 +1,41 @@
 class Solution:
     def numMagicSquaresInside(self, grid: List[List[int]]) -> int:
-        ans = 0
-        m = len(grid)
-        n = len(grid[0])
-        for row in range(m - 2):
-            for col in range(n - 2):
-                if self._isMagicSquare(grid, row, col):
-                    ans += 1
-        return ans
+        def isMagic(i, j):
+            once=[False]*10
+            rowSum=[0]*3
+            colSum= [0]*3
 
-    def _isMagicSquare(self, grid, row, col):
-        seen = [False] * 10
-        for i in range(3):
-            for j in range(3):
-                num = grid[row + i][col + j]
-                if num < 1 or num > 9:
-                    return False
-                if seen[num]:
-                    return False
-                seen[num] = True
+            for a in range(i-1, i+2):
+                for b in range(j-1, j+2):
+                    x= grid[a][b]
+                    if x < 1 or x > 9: return False
+                    rowSum[a-i+1] += x
+                    colSum[b-j+1] += x
+                    if once[x]: 
+                        return False  # it's not unique
+                    once[x]=True
 
-        # Check if diagonal sums are the same
-        diagonal1 = (
-            grid[row][col] + grid[row + 1][col + 1] + grid[row + 2][col + 2]
-        )
-        diagonal2 = (
-            grid[row + 2][col] + grid[row + 1][col + 1] + grid[row][col + 2]
-        )
+            for b in once[1:]: 
+                if not b: return False
 
-        if diagonal1 != diagonal2:
-            return False
+            for sum in rowSum:
+                if sum!=15: return False
+            for sum in colSum:
+                if sum!=15: return False
+            
+            return grid[i-1][j-1]+grid[i+1][j+1]==10 and grid[i+1][j-1]+grid[i-1][j+1]==10
+        
+        r, c = len(grid), len(grid[0])
+        if r < 3 or c < 3: 
+            return 0
 
-        # Check if all row sums are the same as the diagonal sums
-        row1 = grid[row][col] + grid[row][col + 1] + grid[row][col + 2]
-        row2 = (
-            grid[row + 1][col] + grid[row + 1][col + 1] + grid[row + 1][col + 2]
-        )
-        row3 = (
-            grid[row + 2][col] + grid[row + 2][col + 1] + grid[row + 2][col + 2]
-        )
+        cnt=0
+        for i in range(1, r-1):
+            for j in range(1, c-1):
+                if grid[i][j] == 5 and isMagic(i, j): 
+                    cnt+=1
+        return cnt
 
-        if not (row1 == diagonal1 and row2 == diagonal1 and row3 == diagonal1):
-            return False
 
-        # Check if all column sums are the same as the diagonal sums
-        col1 = grid[row][col] + grid[row + 1][col] + grid[row + 2][col]
-        col2 = (
-            grid[row][col + 1] + grid[row + 1][col + 1] + grid[row + 2][col + 1]
-        )
-        col3 = (
-            grid[row][col + 2] + grid[row + 1][col + 2] + grid[row + 2][col + 2]
-        )
 
-        if not (col1 == diagonal1 and col2 == diagonal1 and col3 == diagonal1):
-            return False
-
-        return True
+                
