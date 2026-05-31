2082c68df24de0634213b1161fac436dd5125e74
Tahamid Hossain <mitahamid13@gmail.com>
2026-05-31 23:29:54 +0400

    Time: 64 ms (92.51%), Space: 33.8 MB (75.43%) - LeetHub

---
+++ 2126-destroying-asteroids/2126-destroying-asteroids.py
@@ -0,0 +1,9 @@
+class Solution:
+    def asteroidsDestroyed(self, mass: int, asteroids: List[int]) -> bool:
+        asteroids.sort()  # Sort by mass in ascending order
+        for asteroid in asteroids:
+            # Traverse the asteroids in order, attempt to destroy and update mass or return the result
+            if mass < asteroid:
+                return False
+            mass += asteroid
+        return True  # Successfully destroy all asteroids
